1d3d9828ba6376fcf0903c28445087e225e90f07
zhaoyingming <zhao12051610@163.com>
2018-03-27 22:44:03 +0800

    请求转发与重定向的学习

---
+++ src/main/java/com/zym/forword/Rediect.java
@@ -0,0 +1,27 @@
+package com.zym.forword;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Rediect
+ */
+@WebServlet("/rediect")
+public class Rediect extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("rediect's doget");
+		String path = "testServlet";
+		response.sendRedirect(path);
+	}
+
+}
